e4a75d95a06684d48d53ac9e2b332d15645595bc
Nora Altwaijri <noraaltwaijri@gmail.com>
2019-10-26 13:28:58 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+$(function() {
+  $("table").delegate('td','mouseover mouseout', function(e) {
+    if (e.type == 'mouseover')
+    {
+      $(this).parent().addClass("hover");
+      $("colgroup").eq($(this).index()).addClass("hover");
+    }
+    else
+    {
+      $(this).parent().removeClass("hover");
+      $("colgroup").eq($(this).index()).removeClass("hover");
+    }
+});
+
+});
